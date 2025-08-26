93c3afecc5b07fd39149c66da88b5e6d3384e950
Mahmut Yüksel Mert <mahmutyukselmert@gmail.com>
2025-08-26 08:03:21 +0300

    Bootstrap global dışa aktarıldı

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import "bootstrap";
+import * as bootstrap from "bootstrap";
+window.bootstrap = bootstrap;
+
 import ScrollReveal from "scrollreveal";
 
 document.addEventListener("DOMContentLoaded", function () {
